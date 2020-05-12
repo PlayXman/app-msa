376cd3b819b691d332b0e616fd96d9c7f62d5751
PlayXman <play.man.life@seznam.cz>
2020-05-12 13:26:30 +1000

    New media loader.

--- src/components/MediaComponents/ListLoader.js
+++ src/components/MediaComponents/ListLoader.js
@@ -1,25 +1,39 @@
-import { CircularProgress, withStyles } from '@material-ui/core';
-import React, { PureComponent } from 'react';
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid/Grid';
+import Skeleton from '@material-ui/lab/Skeleton';
 
-const style = {
-	loaderWrapper: {
-		height: '70vh',
-		display: 'flex',
-		alignItems: 'center',
+const useStyles = makeStyles({
+	cont: {
+		width: 158,
 	},
+	row: {
+		marginBottom: '0.5rem',
+	},
+});
+
+const ListLoader = () => {
+	const classes = useStyles();
+
+	return (
+		<React.Fragment>
+			{Array(6)
+				.fill(undefined)
+				.map((val, i) => (
+					<Grid item className={classes.cont} key={i}>
+						<Skeleton
+							className={classes.row}
+							variant="rect"
+							width="100%"
+							height={200}
+						/>
+						<Skeleton className={classes.row} variant="text" />
+						<Skeleton className={classes.row} variant="text" width="50%" />
+						<Skeleton variant="circle" width={20} height={20} />
+					</Grid>
+				))}
+		</React.Fragment>
+	);
 };
 
-/**
- * Loader during all items load
- */
-class ListLoader extends PureComponent {
-	render() {
-		return (
-			<div className={this.props.classes.loaderWrapper}>
-				<CircularProgress />
-			</div>
-		);
-	}
-}
-
-export default withStyles(style)(ListLoader);
+export default ListLoader;
