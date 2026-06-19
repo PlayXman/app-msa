c88ea515deb1ed7ed3e3dc7472155700ca8cb32c
PlayXman <play.man.life@seznam.cz>
2026-06-19 16:13:01 +0200

    refactor: deprecated react event types

--- app/sw.ts
+++ app/sw.ts
@@ -7,6 +7,7 @@ import { Serwist } from "serwist";
 // actual precache manifest. By default, this string is set to
 // `"self.__SW_MANIFEST"`.
 declare global {
+  // noinspection JSUnusedGlobalSymbols
   interface WorkerGlobalScope extends SerwistGlobalConfig {
     __SW_MANIFEST: (PrecacheEntry | string)[] | undefined;
   }
