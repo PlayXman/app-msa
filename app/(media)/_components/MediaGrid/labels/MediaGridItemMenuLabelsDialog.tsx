0ce8b17e17458cfa781c10a8a2da50f496af13e8
PlayXman <play.man.life@seznam.cz>
2026-08-29 18:44:24 +0200

    refactor: unnecessary label updates

--- app/(media)/_components/MediaGrid/labels/MediaGridItemMenuLabelsDialog.tsx
+++ app/(media)/_components/MediaGrid/labels/MediaGridItemMenuLabelsDialog.tsx
@@ -111,14 +111,19 @@ export default function MediaGridItemMenuLabelsDialog({
     const availableLabelUpdates: [string, number][] = [];
     for (const [label, count] of nextLabels.entries()) {
       const initialCount = initialLabels.get(label) ?? 0;
+      const delta = count - initialCount;
 
-      if (count > initialCount) {
+      if (delta === 0) {
+        continue;
+      }
+
+      if (delta > 0) {
         add.push(label);
-      } else if (count < initialCount) {
+      } else {
         remove.push(label);
       }
 
-      availableLabelUpdates.push([label, count - initialCount]);
+      availableLabelUpdates.push([label, delta]);
     }
     const nextModels = mediaItems.map((model) => {
       const nextModel = model.clone();
