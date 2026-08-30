46cab0b8dba52604ca566aa558604469ea212a19
PlayXman <play.man.life@seznam.cz>
2026-08-30 16:53:08 +0200

    feat: remove trakt.tv service integration

--- app/(media)/movies/Movie.ts
+++ app/(media)/movies/Movie.ts
@@ -1,4 +1,3 @@
-import { Trakt } from "@/models/services/Trakt";
 import { Tmdb } from "@/models/services/Tmdb";
 import Media from "@/models/Media";
 import { Props as InfoLink } from "@/app/(media)/_components/MediaGrid/MediaGridItemMenuInfoLink";
@@ -45,11 +44,6 @@ export default class Movie extends Media<VendorIds> {
     return config.vendors.imdbCom.movieSearchUrl + encodeText(this.title);
   }
 
-  async delete(): Promise<void> {
-    await super.delete();
-    await this.removeFromWatchlist();
-  }
-
   async refresh(items: Movie[]): Promise<Movie[]> {
     const tmdb = new Tmdb();
     await Promise.all(
@@ -68,19 +62,5 @@ export default class Movie extends Media<VendorIds> {
     );
 
     return items;
-  }
-
-  async markAsWatched() {
-    const trakt = this.trakt;
-    await trakt.markWatched([this.id]);
-  }
-
-  async removeFromWatchlist() {
-    const trakt = this.trakt;
-    await trakt.removeFromWatchlist([this.id]);
-  }
-
-  protected get trakt(): Trakt {
-    return new Trakt("movies");
   }
 }
